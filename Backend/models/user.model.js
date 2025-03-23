01a9704624b84ddab990f4ace9e0606f99ac8c04
amitdutta <duttaamit356@gmail.com>
2025-03-23 13:41:10 +0530

    Created user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [ 3, 'First name must be at least 3 characters long' ],
+        },
+        lastname: {
+            type: String,
+            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [ 5, 'Email must be at least 5 characters long' ],
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+    },
+})
